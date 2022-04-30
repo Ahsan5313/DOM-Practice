2cb30db7004ee60b1a94a1a6218ed4f00cbd201e
Ahsan <ahsanulhuq17@gmail.com>
2022-04-30 19:48:37 +0600

    dom menopulation

--- js/main.js
+++ js/main.js
@@ -5,3 +5,13 @@ function text(){
   p.innerHTML = 'Text 0';
    
 }
+
+// Day two DOM 
+// If i catch with tag names it will return HTML collection array like object;
+// Let's see;
+// If i catch with class name it will also return HTML collection
+const p = document.getElementById('demo1');
+const allP = document.getElementsByTagName('p');
+const para = document.getElementsByClassName('para')
+console.log(allP);
+console.log(para)
